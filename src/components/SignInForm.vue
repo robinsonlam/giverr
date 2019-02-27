70ec42f8dcd7432816fd92c99351f9be67ac9b7e
Robinson Lam <robinsonlam94@gmail.com>
2019-02-27 18:44:04 +1100

    remove recover password link

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -20,7 +20,6 @@
     </div>
 
     <div class="actions md-layout md-alignment-center-space-between">
-      <a>Reset password</a>
       <md-button class="md-raised md-primary" @click="login">Log in</md-button>
       <md-button class="md-raised md-primary" @click="register">Create Account</md-button>
     </div>
